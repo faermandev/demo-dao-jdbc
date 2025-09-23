8d259c364aa97164f225a396721b40000f0a9c3a
David Faerman <faermandev>
2025-09-23 10:51:21 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,12 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n=== TEST 3: seller findById ===");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
 
     }
 }
